4b246242bfa281d914162b1fd089f77f79200833
HansVilladiego <C1182-23@itmlyceumalabang.onmicrosoft.com>
2025-07-20 01:19:01 +0800

    Add project files.

---
+++ PRELIM_BSIT_31A1_LAB1_Villadiego_Hans/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_BSIT_31A1_LAB1_Villadiego_Hans.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
